4789cb6d12c461172fe22a4b218509b0f7e0981e
szqh97 <szqh97@163.com>
2016-01-03 23:19:21 +0800

    add items for spider

--- zh_spider/items.py
+++ zh_spider/items.py
@@ -12,3 +12,30 @@ class ZhSpiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class zhihuUserInfo (scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    zhihu_id            = scrapy.Field()
+    name                = scrapy.Field()
+    hash_id             = scrapy.Field()
+    bio                 = scrapy.Field()
+    location            = scrapy.Field()
+    business            = scrapy.Field()
+    gender              = scrapy.Field()
+    employment          = scrapy.Field()
+    position            = scrapy.Field()
+    education           = scrapy.Field()
+    education-extra     = scrapy.Field()
+    asks                = scrapy.Field()
+    answers             = scrapy.Field()
+    posts               = scrapy.Field()
+    collections         = scrapy.Field()
+    logs                = scrapy.Field()
+    votes               = scrapy.Field()
+    tanks               = scrapy.Field()
+    favors              = scrapy.Field()
+    shares              = scrapy.Field()
+    followees           = scrapy.Field()
+    followers           = scrapy.Field()
+    pass
