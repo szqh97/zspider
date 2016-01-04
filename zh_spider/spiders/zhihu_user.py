6c293afe78b26ba934aa704b513b798c3b8614f2
szqh97 <szqh97@163.com>
2016-01-04 17:28:47 +0800

    update

--- zh_spider/spiders/zhihu_user.py
+++ zh_spider/spiders/zhihu_user.py
@@ -2,8 +2,10 @@
 """
 TODO
 """
+import os
 import scrapy
 import json
+import time
 from scrapy.http.request import Request
 
 class  ZhihuUserSpider(scrapy.Spider):
@@ -13,11 +15,10 @@ class  ZhihuUserSpider(scrapy.Spider):
     name = "zhihu_user"
     allowed_domains = ["https://zhihu.com"]
 
-    #FIXME start_urls and cookie should passed 
     start_urls = (
-        'https://www.zhihu.com/people/excited-vczh/followees',
+        'https://www.zhihu.com/people/excited-vczh',
         #'https://www.zhihu.com/people/excited-vczh',
-        'https://www.zhihu.com/people/cao-miao-miao-26/followees',
+        'https://www.zhihu.com/people/cao-miao-miao-26',
     )
 
     followees = {}
@@ -27,37 +28,57 @@ class  ZhihuUserSpider(scrapy.Spider):
            'dont_redirect': True,
            'handle_httpstatus_list': [302]
            }
+
     def start_requests(self):
         for url in self.start_urls:
-            self.logger.info("start url is %s", url)
-            #yield Request(url, headers=self.headers, meta=self.meta, callback=self.parse_followee_request)
-            yield Request(url, meta=self.meta, callback=self.parse_followee_request)
+            followees_url = os.path.join(url, 'followees')
+            self.logger.info("followees url is %s", url)
+            yield Request(followees_url, meta=self.meta, callback=self.parse_followees_request)
+
+            followers_url = os.path.join(url, 'followers')
+            self.logger.info('followers url is {0}'.format(followers_url))
+            yield Request(followers_url, meta=self.meta, callback=self.parse_followers_request)
+
+
+    def parse_followers_request(self, response):
+        pass
 
     def parse(self, response):
         pass
 
-    def parse_followee_request(self, response):
-        #self.logger.debug("self.body is %s", response.body)
-
-        # get xsrf for next request
+    def parse_followees_request(self, response):
         user_page = response.xpath('//div[@class="title-section ellipsis"]/a').xpath('@href').extract()[0]
-        self._xsrf = response.xpath('//input[@name="_xsrf"]').xpath("@value").extract()[0]
-        self.logger.debug("xsrf is %s", self._xsrf)
+        _xsrf = response.xpath('//input[@name="_xsrf"]').xpath("@value").extract()[0]
+        self.logger.debug("xsrf is %s", _xsrf)
         str_curr_person_info = response.xpath('//script[@data-name="current_people"]/text()').extract()[0]
-        self.hash_id = json.loads(str_curr_person_info)[-1]
-        self.logger.debug("hash_id is %s", self.hash_id)
+        hash_id = json.loads(str_curr_person_info)[-1]
+        followees_xpth_herf = os.path.join(user_page, 'followees')
+        followers_xpth_herf = os.path.join(user_page, 'followers')
+        followees_number = response.xpath('//div[@class="zm-profile-side-following zg-clear"]/a[@href="{0}"]/strong/text()'.format(followees_xpth_herf)).extract()[0]
+        followers_number = response.xpath('//div[@class="zm-profile-side-following zg-clear"]/a[@href="{0}"]/strong/text()'.format(followers_xpth_herf)).extract()[0]
+
+        self.logger.debug("hash_id is %s", hash_id)
 
         # get top 20 followees
-        tmp_followees = {user_page: []}
+        user_info = {
+                'user_page': user_page,
+                'xsrf': _xsrf,
+                'hash_id': hash_id,
+                'followees': [],
+                'followees_number': int(followees_number),
+                'followers': [],
+                'followers_number': int(followers_number),
+                'created_at': time.strftime('%Y-%m-%d %H:%M:%S'),
+                'updated_at': time.strftime('%Y-%m-%d %H:%M:%S'),
+                }
 
         for p in response.xpath('//h2[@class="zm-list-content-title"]/a'):
             followee = {}
             followee['page_url'] = p.xpath('@href').extract()[0]
             followee['name'] = p.xpath('text()').extract()[0]
             followee['zhihu_id'] = p.xpath('@data-tip').extract()[0].split('p$t$')[-1]
-            tmp_followees[user_page].append(followee)
-            self.logger.debug("followee is %s", str(followee))
+            user_info['followees'].append(followee)
 
-        return tmp_followees
 
+        return user_info
 
